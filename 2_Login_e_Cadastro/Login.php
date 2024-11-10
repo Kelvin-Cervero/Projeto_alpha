dadd259fc0cc5dee2678bd9f5c0c5c85225c808e
ddasni <danielsousamelo5@gmail.com>
2024-11-10 17:22:29 -0300

    Adicionando o codigo da pagina Login

---
+++ 2_Login_e_Cadastro/Login.php
@@ -0,0 +1,49 @@
+<?php
+ // inicinado uma sessão
+    session_start();
+
+    /* Recebendo os dados de login e senha
+       digitados pelo user na pagina de login */ 
+    $login = $_POST['login'];
+    $senha = $_POST['senha'];
+
+    include "conexao.php";
+
+    /*  A variavel $Comando pega as variaveis $login e $senha, 
+        faz uma pesquisa na tablea TB_CLIENTE */
+    $Comando->bindParam(1, $login);
+    $Comando->bindParam(2, $senha);
+    # bindParam: vincula um parâmetro a uma variável em uma instrução SQL preparada
+
+    // Executa a consulta no Banco de Dados;
+    $Comando->execute();
+
+    /* Logo abaixo existe um bloco com if e else, verificando se a variável 
+       $Comando foi bem sucessida, ou seja se ela encontrar algum registro 
+       idêntico o seu valor será igual a 1, se não tiver registros seu valor 
+       será igual a 0. Dependendo do resultado ele redicionará para a página 
+       pagamento.php ou retornara para a página do formulário inicial para 
+       que se possa tentar novamente realizar o login. */
+
+    if ($Comando->rowCount() > 0) {
+        $_SESSION['login'] = $login;
+        $_SESSION['senha'] = $senha;
+
+        header('location:pagamento.php'); # coloque a localização correta
+        exit();
+    }
+    else {
+        unset ($_SESSION['login']);
+        unset ($_SESSION['senha']);
+
+        echo "<script> alert('Usuário e/ou senha incorretos!') </script>";
+        header('location:Login.php');
+        exit();
+    }
+
+    /*
+        header: função que envia um cabeçalho HTTP para o navegador
+        unset: usada para destruir variáveis, elementos de arrays ou objetos (limpar dados)
+        exit: usada para finalizar imediatamente a execução de um script
+    */
+?>
